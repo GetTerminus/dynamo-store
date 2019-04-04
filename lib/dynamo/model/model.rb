0d0ef4417505d80cd5387c94a1b5bcbbf376a5ad
David Harkness <david.harkness@terminus.com>
2019-04-04 14:33:38 -0400

    amme

--- lib/dynamo/model/model.rb
+++ lib/dynamo/model/model.rb
@@ -1,23 +1,26 @@
 # frozen_string_literal: true
 
-class Model
-  DEFAULT_TTL = 60 * 10 # 10 minutes
-  include Dynamoid::Document
+class Dynao
+  class Model
+    DEFAULT_TTL = 60 * 10 # 10 minutes
+    include Dynamoid::Document
 
-  table name: ENV['DYNAMO_TABLE_DYNAMO_CACHE'], key:  :cache_key
+    table name: ENV['DYNAMO_TABLE_DYNAMO_CACHE'], key:  :cache_key
 
-  # Dynamo default field type is string
+    # Dynamo default field type is string
 
-  field :cache_key
-  field :ttl
-  field :cache_value
+    field :cache_key
+    field :ttl
+    field :cache_value
 
-  after_initialize  :set_ttl, unless: :persisted?
+    after_initialize  :set_ttl, unless: :persisted?
 
-  private
+    private
 
-  def set_ttl
-    self.ttl = Time.now.to_i + (ENV['DYNAMO_TABLE_DYNAMO_CACHE_TTL'] || DEFAULT_TTL).to_i
+    def set_ttl
+      self.ttl = Time.now.to_i + (ENV['DYNAMO_TABLE_DYNAMO_CACHE_TTL'] || DEFAULT_TTL).to_i
+    end
+
   end
 
 end
